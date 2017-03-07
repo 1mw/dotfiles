7138cc1d127fb4cacd0d87f346d257c2a39eab9e
Mark Aldrich <maldrich16@simons-rock.edu>
2017-03-07 11:50:08 -0500

    Fixed push script to correctly push lock script

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 # Ensure directories exist
 mkdir -p ./dotfiles/
 mkdir -p ./dotfiles/.config/i3
+mkdir -p ./dotfiles/.bin/scripts
 
 cp --recursive ~/.zshrc ./dotfiles
 cp --recursive ~/.vimrc ./dotfiles
 cp --recursive ~/.config/i3/config ./dotfiles/.config/i3/
-cp --recursive ~/.bin/scripts/lock.sh ./dotfiles
+cp --recursive ~/.bin/scripts/lock.sh ./dotfiles/.bin/scripts/
 cp --recursive ~/.vimperatorrc ./dotfiles
