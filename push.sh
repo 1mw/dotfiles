736335994e1cca16e05e49ecaac5d8346e9f79ee
Mark Aldrich <maldrich16@simons-rock.edu>
2017-03-07 12:04:18 -0500

    Added dunst config

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 # Ensure directories exist
 mkdir -p ./dotfiles/
 mkdir -p ./dotfiles/.config/i3
+mkdir -p ./dotfiles/.config/dunst
 mkdir -p ./dotfiles/.bin/scripts
 
 cp --recursive ~/.zshrc ./dotfiles
@@ -8,3 +9,4 @@ cp --recursive ~/.vimrc ./dotfiles
 cp --recursive ~/.config/i3/config ./dotfiles/.config/i3/
 cp --recursive ~/.bin/scripts/lock.sh ./dotfiles/.bin/scripts/
 cp --recursive ~/.vimperatorrc ./dotfiles
+cp --recursive ~/.config/dunst/dunstrc ./dotfiles
